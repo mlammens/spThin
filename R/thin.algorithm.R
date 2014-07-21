b02e436118d49f084b2f1602460a3f0419eff2c3
Matthew Aiello-Lammens <matt.lammens@gmail.com>
2014-07-21 18:40:17 -0400

    Build and compile of spThin after package name change. This change the names of the Rd files

--- R/thin.algorithm.R
+++ R/thin.algorithm.R
@@ -17,66 +17,98 @@
 #' data.frame of spatially thinned presence records.
 
 thin.algorithm <- function( rec.df.orig, thin.par, reps ) {
-  ##
-  ## General Algorithm:
-  ## ******************
-  ## While at least one member of the nearest neighbor vector is less than 10 km
-  ## remove one of the points associated with nearest neighbor less than 10km randomly
-  ##
-  ## A few more details:
-  ## 1. Define thinning parameter
-  ## 2. Find all points with nearest neighbor closer than thinning parameter
-  ## 3. Randomly delete one of these points
-  ## 4. Continue until no points have nearest neighbor closer than thinning parameter
-  ## 5. Repeat many times 
-  ##      
-  #require( fields )
-  
+
   reduced.rec.dfs <- list()
   
-  # Value for which neighber is too close - i.e. thinning metric
-  print( paste( "Thinning data using thin.par: ", thin.par ) )
-  
   for ( Rep in 1:reps ){
-    #print( paste( "Repetition: ", Rep ) ) ### Debug Line - uncomment to print repetition number
-    rec.df <- rec.df.orig
-
-    # Make distance matrix
+    rec.df <- rec.df.orig    
     DistMat <- rdist.earth(x1=rec.df,miles=FALSE)
-    # print(DistMat) ### DEBUG LINE - warning this could be very large!
-    # Replace diagonal (which is all zeroes) with NAs
     diag(DistMat) <- NA
-    # For each record, calculate nearest neighbor
-    NearNeighbor <- apply( DistMat, MARGIN=2, FUN=min, na.rm=TRUE )
-    MinNearNeighbor <- min(NearNeighbor)
-    # print(MinNearNeighbor) ### DEBUG LINE
     
-    while( MinNearNeighbor<thin.par & nrow(rec.df)>1 ){
+    while(min(DistMat, na.rm=TRUE) < thin.par & nrow(rec.df) > 1) {      
+      CloseRecs <- which(DistMat < thin.par, arr.ind=TRUE)[, 1]
+      RemoveRec <- as.numeric(names(which(table(CloseRecs) == max(table(CloseRecs)))))
       
-      # Determine records with a minimum nearest neighbor distance less than thin.par 
-      CloseRecs <- which( NearNeighbor < thin.par )
-      # print( paste( 'Length of CloseRecs: ',length(CloseRecs)) ) ### DEBUG LINE
-      
-      # Choose one record to remove randomly
-      RemoveRec <- sample(CloseRecs,1)
-      # print( paste( 'RemoveRec: ',RemoveRec) ) ### DEBUG LINE
-      
-      # Remove random record from the complete records data.frame rec.df
-      rec.df <-rec.df[ -RemoveRec, ]
-      # print( paste( 'Number of remaining records: ', nrow(rec.df) )) ### DEBUG LINE
-      
-      # Remove random record from the distance matrix DistMat
-      DistMat <- DistMat[ -RemoveRec, -RemoveRec ]
-
-      # Calculate NEW nearest neighbors **UNLESS* there is only one location
-      # remaining. 
-      if ( nrow(rec.df)>1 ){
-        NearNeighbor <- apply( DistMat, MARGIN=2, FUN=min, na.rm=TRUE )
-        MinNearNeighbor <- min(NearNeighbor)
-        # print(paste( 'MinNearNeighbor inside loop: ', MinNearNeighbor) ) DEBUG LINE
+      if(length(RemoveRec) > 1) {
+        RemoveRec <- sample(RemoveRec, 1)
       }
+      
+      rec.df <-rec.df[ -RemoveRec, ]
+      DistMat <- DistMat[ -RemoveRec, -RemoveRec ]     
     }
     reduced.rec.dfs[[Rep]] <- rec.df
   }
   return( reduced.rec.dfs )
 }
+
+
+## ******************************************************************** ##
+## Below is legacy code that is temporarly being stored in this file.
+## After a round of peer-review, we recieved a suggestion that in how
+## the algorithm works that fundamentally changed the alogirthm and
+## increased its efficiency by orders of magnitude.
+## The code below remains so that MA-L can incorporate comments
+## appropriately to the new code.
+## ******************************************************************** ##
+#   ##
+#   ## General Algorithm:
+#   ## ******************
+#   ## While at least one member of the nearest neighbor vector is less than 10 km
+#   ## remove one of the points associated with nearest neighbor less than 10km randomly
+#   ##
+#   ## A few more details:
+#   ## 1. Define thinning parameter
+#   ## 2. Find all points with nearest neighbor closer than thinning parameter
+#   ## 3. Randomly delete one of these points
+#   ## 4. Continue until no points have nearest neighbor closer than thinning parameter
+#   ## 5. Repeat many times 
+#   ##      
+#   
+#   reduced.rec.dfs <- list()
+#   
+#   # Value for which neighber is too close - i.e. thinning metric
+#   print( paste( "Thinning data using thin.par: ", thin.par ) )
+#   
+#   for ( Rep in 1:reps ){
+#     #print( paste( "Repetition: ", Rep ) ) ### Debug Line - uncomment to print repetition number
+#     rec.df <- rec.df.orig
+# 
+#     # Make distance matrix
+#     DistMat <- rdist.earth(x1=rec.df,miles=FALSE)
+#     # print(DistMat) ### DEBUG LINE - warning this could be very large!
+#     # Replace diagonal (which is all zeroes) with NAs
+#     diag(DistMat) <- NA
+#     # For each record, calculate nearest neighbor
+#     NearNeighbor <- apply( DistMat, MARGIN=2, FUN=min, na.rm=TRUE )
+#     MinNearNeighbor <- min(NearNeighbor)
+#     # print(MinNearNeighbor) ### DEBUG LINE
+#     
+#     while( MinNearNeighbor<thin.par & nrow(rec.df)>1 ){
+#       
+#       # Determine records with a minimum nearest neighbor distance less than thin.par 
+#       CloseRecs <- which( NearNeighbor < thin.par )
+#       # print( paste( 'Length of CloseRecs: ',length(CloseRecs)) ) ### DEBUG LINE
+#       
+#       # Choose one record to remove randomly
+#       RemoveRec <- sample(CloseRecs,1)
+#       # print( paste( 'RemoveRec: ',RemoveRec) ) ### DEBUG LINE
+#       
+#       # Remove random record from the complete records data.frame rec.df
+#       rec.df <-rec.df[ -RemoveRec, ]
+#       # print( paste( 'Number of remaining records: ', nrow(rec.df) )) ### DEBUG LINE
+#       
+#       # Remove random record from the distance matrix DistMat
+#       DistMat <- DistMat[ -RemoveRec, -RemoveRec ]
+# 
+#       # Calculate NEW nearest neighbors **UNLESS* there is only one location
+#       # remaining. 
+#       if ( nrow(rec.df)>1 ){
+#         NearNeighbor <- apply( DistMat, MARGIN=2, FUN=min, na.rm=TRUE )
+#         MinNearNeighbor <- min(NearNeighbor)
+#         # print(paste( 'MinNearNeighbor inside loop: ', MinNearNeighbor) ) DEBUG LINE
+#       }
+#     }
+#     reduced.rec.dfs[[Rep]] <- rec.df
+#   }
+#   return( reduced.rec.dfs )
+# }
