e8ec6267c7f372002742186c6f808a5622694a46
Matthew Aiello-Lammens <matt.lammens@gmail.com>
2014-07-25 20:18:49 +0200

    Added region identification to dataset. This facilitates seperation into multiple regions.

--- R/Heteromys_anomalus_South_America.R
+++ R/Heteromys_anomalus_South_America.R
@@ -9,10 +9,11 @@
 #'   \item SPEC. species name assigned to occurrence record  
 #'   \item LAT. decimal degree latitude value
 #'   \item LONG. decimal degree longitude value 
+#'   \item REGION. region, or island, of occurrence
 #' }
 #' 
 #' @docType data
 #' @keywords datasets
-#' @format A data frame with 201 rows and 3 variables
+#' @format A data frame with 201 rows and 4 variables
 #' @name Heteromys_anomalus_South_America
 NULL
