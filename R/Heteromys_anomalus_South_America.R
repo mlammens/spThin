19f02f9cd01d2d94425746316b7607ff75c95edb
Matthew Aiello-Lammens <matt.lammens@gmail.com>
2014-07-25 17:41:41 +0200

    Added the Heteromyn dataset and description for this dataset to the package.

---
+++ R/Heteromys_anomalus_South_America.R
@@ -0,0 +1,18 @@
+#' Occurrence record locations for Heteromys anamalus
+#' 
+#' 
+#' A dataset containing compiled occurrence record locations for 
+#' Heteromys anomalus in northern coastal South America. These records
+#' have been examined to check for accurate species identification.
+#' 
+#' \itemize{
+#'   \item SPEC. species name assigned to occurrence record  
+#'   \item LAT. decimal degree latitude value
+#'   \item LONG. decimal degree longitude value 
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 201 rows and 3 variables
+#' @name Heteromys_anomalus_South_America
+NULL
